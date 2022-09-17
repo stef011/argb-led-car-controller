c2333b4217fcf03066f41bcfb52028af25434b4d
Stéphane Zundel <stephane.zundel@viacesi.fr>
2022-09-17 18:50:44 +0200

    First Commit 6 functional effects.

---
+++ build/sketch/config.h
@@ -0,0 +1,28 @@
+#line 1 "c:\\Users\\Stephane\\Documents\\Arduino\\LEDController\\config.h"
+#ifndef Config_H
+#define Config_H
+
+#include <Arduino.h>
+#include <FastLED.h>
+
+#define LED_PIN 2
+#define NUM_LEDS 223
+#define MAX_BRIGHTNESS 200
+#define FRAMES_PER_SECOND 10
+
+#define BUTTON_PLUS_PIN 5
+#define BUTTON_MINUS_PIN 4
+
+#define POT_BRIGHTNESS A1
+#define POT_COLOR A2
+#define POT_DELAY A3
+#define POT_MISC A4
+
+#define EFFECTS_NUM 6
+
+extern CRGB leds[NUM_LEDS];
+extern int tableState;
+extern bool isInitialized;
+extern int brightness;
+
+#endif
---
+++ inputs.h
@@ -0,0 +1,17 @@
+#ifndef Inputs_H
+#define Inputs_H
+
+#include <Arduino.h>
+#include <FastLED.h>
+#include "config.h"
+
+
+CHSV potToCHSV(int pot);
+
+bool readButton(int pin);
+
+bool checkForChange(bool current, bool last);
+
+int cycleCounter(int cycle, bool reverse);
+
+#endif
