24c9b3c76152277c491e58b597c5ef31f23c0c08
Stéphane Zundel <stephane.zundel@viacesi.fr>
2022-10-16 11:54:58 +0200

    Fixed Back and Forth effect, closing #3
    Also added Brightness control to ColorWipe and BackAndForth effects.

--- src/effects/BackAndForth.cpp
+++ src/effects/BackAndForth.cpp
@@ -2,7 +2,7 @@
 
 void BackAndForth::tick(LEDStrip &ledStrip, Parameters *parameters)
 {
-  uint8_t thisLed = map(beat8(parameters->getSpeed()), 0, 255, 0, ledStrip.getNumLeds() - 1);
-  ledStrip.leds[thisLed] = parameters->getColor();
+  uint8_t thisLed = beatsin16(parameters->getSpeed(), 0, ledStrip.getNumLeds() - 1);
+  ledStrip.leds[thisLed] = CHSV(parameters->getColor().hue, 255, parameters->getBrightness());
   fadeToBlackBy(ledStrip.leds, ledStrip.getNumLeds(), max(1, parameters->getFade()));
 };
--- src/effects/ColorWipe.cpp
+++ src/effects/ColorWipe.cpp
@@ -3,6 +3,6 @@
 void ColorWipe::tick(LEDStrip &ledStrip, Parameters *parameters)
 {
   uint8_t thisLed = map(beat8(parameters->getSpeed()), 0, 255, 0, ledStrip.getNumLeds() - 1);
-  ledStrip.leds[thisLed] = parameters->getColor();
+  ledStrip.leds[thisLed] = CHSV(parameters->getColor().hue, 255, parameters->getBrightness());
   fadeToBlackBy(ledStrip.leds, ledStrip.getNumLeds(), max(1, parameters->getFade()));
 }
